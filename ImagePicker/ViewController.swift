90aceb938ca49d079341ad51b6ed2504740c7f03
Kynan <k.song@blueyonder.co.uk>
2018-05-12 09:44:28 +0100

    Worked through a solution and made notes.

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,22 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    let pickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerController.delegate = self
+        
         
     }
     
-
     @IBAction func pickImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
+        pickerController.allowsEditing = false
+        pickerController.sourceType = .photoLibrary
         present(pickerController, animated: true, completion: nil)
         //used to present the image picker.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.contentMode = .scaleAspectFit
+            //Tells the image to fit to the view.
+            imagePickerView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+        // Dismisses image picker/ modally presented view.
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        //Cancels image picker when user presses Cancel.
+    }
+    
+    //Learnt from Udacity and http://www.codingexplorer.com/choosing-images-with-uiimagepickercontroller-in-swift/
     
 }
 
